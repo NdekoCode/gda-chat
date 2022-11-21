ad6a4f0971337bbe52364b90510119ee6a116f09
Ndekocode <arickbulakali@gmail.com>
2022-11-21 07:22:33 +0200

    add: main auth function

---
+++ api/controllers/UserCTRL.js
@@ -0,0 +1,20 @@
+export default class UserCTRL {
+  /**
+   * @description Pour l'enregistrement des nouveaux utilisateurs
+   * @author NdekoCode
+   * @param {IncomingMessage} req
+   * @param {ServerResponse} res
+   * @param {Function} next
+   * @memberof UserCTRL
+   */
+  signup(req, res, next) {}
+  /**
+   * @description Pour connecter des utilisateurs existants
+   * @author NdekoCode
+   * @param {IncomingMessage} req
+   * @param {ServerResponse} res
+   * @param {Function} next
+   * @memberof UserCTRL
+   */
+  login(req, res, next) {}
+}
