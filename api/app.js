ad63b960a4b99a9a1a55d2a63b4f718ba1c1f2e5
Ndekocode <arickbulakali@gmail.com>
2022-11-21 07:27:00 +0200

    (api): feat: load auth routes login and signup

--- api/app.js
+++ api/app.js
@@ -1,5 +1,7 @@
 import express from "express";
+import userRoutes from "./routes/user.routes.js";
 const app = express();
 app.use(express.json());
+app.use("/api/auth", userRoutes);
 app.use(express.urlencoded({ extended: true }));
 export default app;
