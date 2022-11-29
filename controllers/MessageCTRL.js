0e18a76c538ff29ebee953795d0435d9b0ca2763
Ndekocode <arickbulakali@gmail.com>
2022-11-29 18:10:21 +0200

    refactor: addMessage to testing if receiver exists

--- controllers/MessageCTRL.js
+++ controllers/MessageCTRL.js
@@ -1,13 +1,23 @@
 import MessageMDL from "../models/MessageMDL.js";
+import UserMDL from "../models/UserMDL.js";
 import Alert from "../utils/Alert.js";
 import { isEmpty, validForm } from "../utils/validators.js";
 
 export default class MessageCTRL {
-  async home(req, res, next) {
+  /**
+   *
+   * @description Récupère tous les messages que l'utilisateurs connecter à déjà envoyer ou qu'il a déjà recus
+   * @author NdekoCode
+   * @param {IncomingMessage} req
+   * @param {ServerResponse} res
+   * @return {array<object>}
+   * @memberof MessageCTRL
+   */
+  async home(req, res) {
     const alert = new Alert(req, res);
     try {
       const messages = await MessageMDL.find({
-        $or: [{ sender: req.auth.userId }, { receiver: req.auth.userId }],
+        $or: [{ senderId: req.auth.userId }, { receiver: req.auth.userId }],
       });
       if (messages !== null) {
         return res.send(messages);
@@ -17,21 +27,40 @@ export default class MessageCTRL {
       return alert.danger("Erreurs lors de la récuperation des messages");
     }
   }
+  /**
+   * @description Ajoute un message dans la base de donnée
+   * @author NdekoCode
+   * @param {IncomingMessage} req
+   * @param {ServerResponse} res
+   * @return {void}
+   * @memberof MessageCTRL
+   */
   async addMessage(req, res) {
     const alert = new Alert(req, res);
+    const senderId = req.auth.userId;
+    const receiverId = req.params.id;
+    const talkersIds = [senderId, receiverId];
     const bodyRequest = {
       ...req.body,
-      receiverId: req.params.id,
-      senderId: req.auth.userId,
-      talkersIds: [req.params.id, req.auth.userId],
+      receiverId,
+      senderId,
+      talkersIds,
     };
     const errors = validForm(bodyRequest);
     console.log(bodyRequest, errors);
     if (isEmpty(errors)) {
       try {
-        const chat = new MessageMDL(bodyRequest);
-        await chat.save();
-        return alert.success("Messages ajouter avec succès");
+        const userExist = await UserMDL.findById(receiverId);
+        if (userExist !== null) {
+          const chat = new MessageMDL(bodyRequest);
+          await chat.save();
+          return alert.success("Messages ajouter avec succès");
+        }
+
+        return alert.danger(
+          "L'utilisateur à qui vous voulez envoyer le message n'existe pas",
+          404
+        );
       } catch (error) {
         return alert.danger(
           "Erreur survenus lors de l'envois du message " + error.message
@@ -39,7 +68,7 @@ export default class MessageCTRL {
       }
     }
 
-    return alert.danger(errors);
+    return alert.danger(errors["error"]);
   }
   async getChatUser(req, res, next) {
     const alert = new Alert(req, res);
