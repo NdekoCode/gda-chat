71b09200f584b44ce7c03cf0c6b2235a46fd9358
Ndekocode <arickbulakali@gmail.com>
2023-01-26 15:50:21 +0200

    init testing app

---
+++ test/setup.test.js
@@ -0,0 +1,19 @@
+import server from "../app.js";
+import MessageMDL from "../models/MessageMDL.js";
+import UserMDL from "../models/UserMDL.js";
+
+chai.use(chaiHttp);
+//clean up the database before and after each test
+beforeEach((done) => {
+  MessageMDL.deleteMany({}, function (err) {});
+  UserMDL.deleteMany({}, function (err) {});
+  done();
+});
+afterEach((done) => {
+  MessageMDL.deleteMany({}, function (err) {});
+  UserMDL.deleteMany({}, function (err) {});
+  done();
+});
+
+const request = chai.request(server).keepOpen();
+export default request;
